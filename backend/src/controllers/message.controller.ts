669aceb51a7c1fef48900140b21afc45b71013ea
BentlyM <metayerbently@gmail.com>
2024-09-30 15:50:09 -0400

    "Added protectRoute middleware and implemented getMessages, getUserForSidebar, and sendMessage controllers in message.route.ts"

---
+++ backend/src/controllers/message.controller.ts
@@ -0,0 +1,115 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient()
+
+export const sendMessage = async (req: Request, res: Response) => {
+    try{
+        const {message} = req.body;
+        const {id: receiverId} = req.params;
+        const senderId = req.user.id;
+
+        let conversation = await prisma.conversation.findFirst({
+            where: {
+                participantIds: {
+                    hasEvery: [senderId, receiverId],
+                }
+            }
+        }) 
+
+        if(!conversation){
+            conversation = await prisma.conversation.create({
+                data: {
+                    participantIds: {
+                        set: [senderId, receiverId]
+                    }
+                }
+            })
+        }
+
+        const newMessage = await prisma.message.create({
+            data: {
+                senderId,
+                body: message,
+                conversationId: conversation.id
+            }
+        })
+
+        if(newMessage){
+            conversation = await prisma.conversation.update({
+                where: {
+                    id: conversation.id,
+                },
+                data: {
+                    messages: {
+                        connect: {
+                            id: newMessage.id
+                        }
+                    }
+                }
+            })
+        }
+
+
+        // socket io will go here
+        return res.json(201).json(newMessage);
+    }catch(error: any){
+        console.error("Error in sendMessage", error.message);
+        return res.status(500).json({error: "Internal server error"})
+    }
+}
+
+export const getMessages = async (req: Request, res: Response) => {
+    try{
+        const {id: userToChatId} = req.params;
+        const senderId = req.user.id;
+
+        const conversation = await prisma.conversation.findFirst({
+            where: {
+                participantIds: {
+                    hasEvery: [senderId, userToChatId]
+                }
+            },
+            include: {
+                messages: {
+                    orderBy: {
+                        createdAt: 'asc'
+                    }
+                }
+            }
+        })
+
+        if(!conversation){
+            return res.status(200).json([])
+        }
+
+        return res.status(200).json(conversation.messages);
+    }catch(error: any){
+        console.error("Error in getMessages: ", error.message);
+        return res.status(500).json({error: "Internal server error"})
+    }
+}
+
+export const getUserForSidebar = async (req: Request, res: Response) => {
+    try{
+        const authUserId = req.user.id
+
+        const users = await prisma.user.findMany({
+            where: {
+                id: {
+                    not: authUserId
+                }
+            },
+            select: {
+                id: true,
+                fullName: true,
+                profilePic: true,
+            }
+        })
+
+        return res.status(200).json(users);
+    }catch(error: any){
+        console.error("Error in getUserForSidebar", error.message);
+        return res.status(500).json({error: "Internal server error"});
+    }
+}
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import protectRoute from '../middleware/protectRoute';
+import { getMessages, getUserForSidebar, sendMessage } from '../controllers/message.controller';
 
 const router = express.Router();
 
-router.get('/conversations', (req, res) => {
-    res.send("conversation route");
-})
+
+router.get("/conversations", protectRoute, getUserForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 
 export default router;
