6be8251a5a688635657b5b25981b64d42013a93a
BentlyM <metayerbently@gmail.com>
2024-10-04 00:46:15 -0400

    updated deployment init 7

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:5000',
+                target: 'https://chat-app-0yco.onrender.com',
                 changeOrigin: true,
             },
         },
