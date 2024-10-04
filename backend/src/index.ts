91db904ac237edfb25c93749480e8e8b2eb08e1a
BentlyM <metayerbently@gmail.com>
2024-10-04 00:24:18 -0400

    updated deployment init 5

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,9 +34,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 if (process.env.NODE_ENV !== 'development') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.use(express.static(path.join(__dirname, '../../frontend/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../frontend', 'dist', 'index.html'));
   });
 }
 
